771d5eff468c6d0b0c43455efcae7edb74ec66b2
Cstoscup <109521820+Cstoscup@users.noreply.github.com>
2022-09-10 20:04:23 -0400

    Added clear drawing button

--- app.js
+++ app.js
@@ -1,4 +1,8 @@
 const container = document.querySelector("#container");
+let gridSize = 16;
+let mouseDown = false;
+document.body.onmousedown = () => (mouseDown = true);
+document.body.onmouseup = () => (mouseDown = false);
 
 function drawGrid(width) {
   let rows = 0;
@@ -13,6 +17,7 @@ function drawGrid(width) {
       const square = document.createElement("div");
       square.classList.add("square");
       square.addEventListener('mouseover', test);
+      square.addEventListener('mousedown', test);
       row.appendChild(square);
       squares++;
     }
@@ -20,19 +25,30 @@ function drawGrid(width) {
 }
 
 function setGridSize() {
-  let width = prompt("Enter a grid width less than 100.");
-  if (width > 99) {
-    width = prompt("Enter a width less than 100.");
+  gridSize = prompt("Enter a grid width less than 100.");
+  if (gridSize > 99) {
+    gridSize = prompt("Enter a width less than 100.");
   }
   container.innerHTML = '';
-  drawGrid(width);
+  drawGrid(gridSize);
+}
+
+function clearDrawing() {
+  container.innerHTML = '';
+  drawGrid(gridSize);
 }
 
 function test(event) {
+  if (event.type === 'mouseover' && !mouseDown) {
+    return;
+  }
   event.target.classList.add("sketch");
 }
 
-const btn = document.querySelector("#size-button");
-btn.addEventListener("click", setGridSize);
+const gridSizeBtn = document.querySelector("#size-button");
+gridSizeBtn.addEventListener("click", setGridSize);
 
-drawGrid(16);
+const clearBtn = document.querySelector("#clear-button");
+clearBtn.addEventListener("click", clearDrawing);
+
+drawGrid(gridSize);
